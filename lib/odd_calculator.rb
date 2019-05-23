f027d795ec591c8a7c7465e64a5fc26db803a2bd
Adrian Peterson Co <adrianpetersonco@gmail.com>
2019-05-23 17:07:01 +0800

    Commits everything before I remake the formula again

--- lib/odd_calculator.rb
+++ lib/odd_calculator.rb
@@ -1,20 +1,19 @@
 class Participant
-  attr_accessor :name, :seed, :odds
+  attr_accessor :name, :odds
 
   class << self
-    def for(item, index = nil)
+    def for(item, odds)
       case item.class.to_s
         when "String"
-          new(name: item, seed: index)
-        when "Hash"
-          new(name: item[:name], seed: item[:seed])
+          new(name: item, odds: odds) when "Hash"
+          new(name: item[:name], odds: item[:odds])
       end
     end
   end
 
-  def initialize(name:, seed: 0, options: {})
+  def initialize(name:, odds: 0, options: {})
     @name = name
-    @seed = seed
+    @odds = odds
     @options = options
   end
 end
@@ -35,14 +34,41 @@ class OddCalculator
 
   def even_odds
     @collection.map.with_index do |item, index|
-      p = Participant.for(item, index)
-      p.odds = @collection.length.to_f
-
-      p
+      Participant.for(item, @collection.length.to_f)
     end
   end
 
   def seeded_odds
-    []
+    median = 100 / @collection.length.to_f 
+    divided_collection = @collection.each_slice((@collection.length / 2.0).round).to_a
+
+    do_seeded_odds({
+      collection: divided_collection[0].reverse,
+      operation: "+",
+      prev_odds: @collection.length % 2 == 0 ? median : 0,
+      median: median,
+      acc: []
+    }).reverse + 
+    do_seeded_odds({
+      collection: divided_collection[1],
+      operation: "-",
+      prev_odds: @collection.length % 2 == 0 ? 0 : median,
+      median: median,
+      acc: []
+    })
+  end
+
+  def do_seeded_odds(collection:, operation:, prev_odds:, median:, acc:)
+    return acc if collection.empty?
+
+    acc << Participant.for(collection.shift, prev_odds.zero? ? median : median.send(operation, (prev_odds / 2)))
+
+    do_seeded_odds({
+      collection: collection,
+      operation: operation,
+      prev_odds: acc.last.odds,
+      median: median,
+      acc: acc
+    })
   end
 end
--- spec/lib/odd_calculator_spec.rb
+++ spec/lib/odd_calculator_spec.rb
@@ -26,20 +26,49 @@ RSpec.describe OddCalculator, type: :lib do
   end
 
   describe '#seeded_odds' do
-    let!(:collection) { ['A', 'B', 'C'] }
+    context 'when the collection length is odd' do
+      let!(:collection) { ['A', 'B', 'C', 'D', 'E'] }
 
-    it 'calculate the odds based on the seed level that was given' do
-      result = lib.calculate(:seeded_odds)
-      expect(result).to be_instance_of Array
+      it 'calculate the odds based on the seed level that was given' do
+        result = lib.calculate(:seeded_odds)
 
-      expect(result[0].name).to eq "A"
-      expect(result[0].odds).to eq 
+        expect(result).to be_instance_of Array
 
-      expect(result[1].name).to eq "B"
-      expect(result[1].odds).to eq 
+        expect(result[0]).to be_instance_of Participant
+        expect(result[0].name).to eq "A"
+        expect(result[0].odds).to eq 35.00
+        expect(result[1].odds).to eq 30.00
+        expect(result[2].odds).to eq 20.00
+        expect(result[3].odds).to eq 10.00
+        expect(result[4].odds).to eq 5.00
+      end
+    end
 
-      expect(result[2].name).to eq "C"
-      expect(result[2].odds).to eq 
+    context 'when the collection length is even' do
+      let!(:collection) { ['A', 'B', 'C', 'D', 'E', 'F'] }
+
+      it 'calculate the odds based on the seed level that was given' do
+        result = lib.calculate(:seeded_odds)
+  
+        expect(result).to be_instance_of Array
+
+        expect(result[2]).to be_instance_of Participant
+        expect(result[2].name).to eq "C"
+        expect(result[3]).to be_instance_of Participant
+        expect(result[3].name).to eq "D"
+      end
+    end
+
+    context 'when the collection has more numbers' do
+      let!(:collection) { ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'] }
+
+      it 'calculates the odds' do
+        result = lib.calculate(:seeded_odds)
+
+        expect(result).to be_instance_of Array
+
+        puts result.to_yaml
+      end
     end
   end
 
@@ -62,7 +91,7 @@ RSpec.describe OddCalculator, type: :lib do
     end
 
     context 'when a collection was passed' do
-      let!(:collection) { [ {name: 'A', seed: 1}, {name: 'B', seed: 2}, {name: 'C', seed: 3}, {name: 'D', seed: 4} ] }
+      let!(:collection) { [ {name: 'A', odds: 1}, {name: 'B', odds: 2}, {name: 'C', odds: 3}, {name: 'D', odds: 4} ] }
 
       it 'calculates the odds for each participant in decimal format based on the seeding allocation' do
         result = lib.calculate
@@ -70,11 +99,11 @@ RSpec.describe OddCalculator, type: :lib do
 
         expect(result[0]).to be_instance_of Participant
         expect(result[0].name).to eq "A"
-        expect(result[0].odds).to eq 4.00
+        expect(result[0].odds).to eq 1
 
         expect(result[3]).to be_instance_of Participant
         expect(result[3].name).to eq "D"
-        expect(result[3].odds).to eq 4.00
+        expect(result[3].odds).to eq 4
       end
     end
   end
